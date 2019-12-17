b67d93211c5f99b4fe2817536516a94228cc0d31
yattrium <wolf.mikoto@gmail.com>
2019-12-17 22:36:11 +0300

    16 лаба(а то в первый раз было не то)

--- Lab16/Lab16.5/Program.cs
+++ Lab16/Lab16.5/Program.cs
@@ -13,7 +13,7 @@ namespace Lab16._5
         {
             Console.Write("введите размер массива ");
             int N = Convert.ToInt32(Console.ReadLine());
-            int[] arr = new int[100];
+            int[] arr = new int[N];
             //int k=0,p=0;
             for (int i = 0; i < N; i++)
             {
@@ -21,27 +21,35 @@ namespace Lab16._5
                 arr[i] = Convert.ToInt32(Console.ReadLine());
                // if (arr[i] > 0) k++;
             }
-           // int[] arr2 = new int[N + k];
-
-            for (int i = N - 1; i >= 0; i--)
-            {
-                if (arr[i] > 0)
-                {
-                    N++;
-                    for (int j = N - 1; j > i; j--)
-                    {
-                        arr[j] = arr[j - 1];
-                    }
-                    arr[i] = 0;
-                }
-            }
 
             for (int i = 0; i < N; i++)
             {
-                Console.WriteLine(arr[i]);
+                if (arr[i] > 0) Console.Write("0 " + arr[i] + " ");
+                else Console.Write(arr[i] + " ");
             }
 
             Console.ReadLine();
+
+            // int[] arr2 = new int[N + k];
+
+            /* for (int i = N - 1; i > 0; i--)
+             {
+                 if (arr[i] > 0)
+                 {
+                     N++;
+                     for (int j = N - 1; j > i; j--)
+                     {
+                         arr[j] = arr[j - 1];
+                     }
+                     arr[i] = 0;
+                 }
+             }
+
+             for (int i = 0; i < N; i++)
+             {
+                 Console.WriteLine(arr[i]);
+             } */
+
         }
     }
 }
